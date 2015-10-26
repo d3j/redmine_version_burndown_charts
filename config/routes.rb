ab09c60b9262c75d12578b3b20b915141f518aa4
Your Name <you@example.com>
2015-10-26 09:37:37 -0700

    for Rails 4.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 RedmineApp::Application.routes.draw do
-  match 'version_burndown_charts/:project_id' => 'version_burndown_charts#index'
-  match 'version_burndown_charts/get_graph_data/:project_id' => 'version_burndown_charts#get_graph_data'
+  get 'version_burndown_charts/:project_id' => 'version_burndown_charts#index'
+  get 'version_burndown_charts/get_graph_data/:project_id' => 'version_burndown_charts#get_graph_data'
 end
